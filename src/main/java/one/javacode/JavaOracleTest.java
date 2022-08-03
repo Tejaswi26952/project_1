ddcdf0c7352f929d755ff40e4d8be41075127e1b
amdjsfk44 <amdjsfk44@Amdjfs115.iiht.tech>
2022-08-03 10:32:33 +0530

    second commit

--- src/main/java/one/javacode/JavaOracleTest.java
+++ src/main/java/one/javacode/JavaOracleTest.java
@@ -19,24 +19,7 @@ public class JavaOracleTest {
 		    
 			Connection connection = DriverManager.getConnection(URL, username, password);
 			System.out.println("Connected successfully");
-			String sql ="INSERT INTO bankaccounts(customername,city)" + "VALUES('Tejaswi','Guntur')";
-			String sql1 ="INSERT INTO bankaccounts(customername,city)" + "VALUES('sreeya','vijayawada')";
-			String sql2 ="INSERT INTO bankaccounts(customername,city)" + "VALUES('Divya','ongoole')";
-			String sql3 ="INSERT INTO bankaccounts(customername,city)" + "VALUES('vindhya','nellore')";
-			Statement statement =connection.createStatement();
-			Statement statement1 =connection.createStatement();
-			Statement statement2 =connection.createStatement();
-			Statement statement3 =connection.createStatement();
-			int rows =statement.executeUpdate(sql);
-			int rows1 =statement.executeUpdate(sql1);
-			int rows2 =statement.executeUpdate(sql2);
-			int rows3 =statement.executeUpdate(sql3);
-
-			int row = 0;
-			if (row > 0) {
-				System.out.println("A row has been inserted");
-			}
-			connection.close();
+			
 			
 		} catch (ClassNotFoundException e) {
 			  e.printStackTrace();
